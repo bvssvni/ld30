642543a59f2fa2753660a302047715a78f0bd224
Sven Nilsen <bvssvni@gmail.com>
2014-08-23 09:04:55 +0200

    Added window and physics world

--- ld30/src/main.rs
+++ ld30/src/main.rs
@@ -2,9 +2,36 @@
 extern crate piston;
 extern crate gfx;
 extern crate nphysics3df32;
+extern crate sdl2_game_window;
 
 use nphysics3df32 as phys;
+use sdl2_game_window::GameWindowSDL2 as Window;
 
 fn main() {
-    println!("hello!");
+    let mut world = phys::world::World::new();
+    let mut window = Window::new(
+        piston::shader_version::opengl::OpenGL_3_2,
+        piston::GameWindowSettings {
+            title: "Ludum Dare 30".to_string(),
+            size: [640, 480],
+            exit_on_esc: true,
+            fullscreen: false,
+        }
+    );
+
+    let game_iter_settings = piston::GameIteratorSettings {
+        updates_per_second: 120,
+        max_frames_per_second: 60,
+    };
+    for e in piston::GameIterator::new(&mut window, &game_iter_settings) {
+        match e {
+            piston::Render(_args) => {},
+            piston::Update(args) => {
+                // Update physics.
+                world.step(args.dt as f32);
+            },
+            piston::Input(_args) => {}
+        }
+    }
 }
+
